d462d2d9ebb1e9afb89db99f95515f8d5500bc22
Crowdbotics <team@crowdbotics.com>
2024-09-07 18:09:00 +0000

    Initial commit

---
+++ backend/manalife_49321/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for manalife_49321 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "manalife_49321.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "manalife-49321",
+  url: "https://manalife-49321.botics.co",
+  api: "https://manalife-49321.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
